ac6eaf2e0ff9063c6c3dce5ebe0f7855140ed6a5
ssimoanto <simoanto@ucm.es>
2023-04-21 12:46:20 +0200

    RV

---
+++ Assets/Scripts/SwitchActive.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SwitchActive : MonoBehaviour
+{
+    public float maxTime = 0.5f;
+    float last = 0.0f;
+    float current = 0.0f;
+    bool switchy = true;
+    //GameObject gO;
+    private void Start()
+    {
+        current = Time.time;
+        last = current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current = Time.time;
+        Debug.Log(Time.time);
+        if (current >= last + maxTime)
+        {
+            switchy = !switchy;
+            this.transform.gameObject.GetComponent<Light>().enabled = switchy;
+            last = current;
+        }
+    }
+}
